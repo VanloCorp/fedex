38dc9af2eb1bbe4674e8b279c779bc24b7c4a6ac
Sawyer <sawyerbateman@gmail.com>
2021-05-07 22:59:32 +0000

    Add surcharges to rate response.

--- lib/fedex/rate.rb
+++ lib/fedex/rate.rb
@@ -18,7 +18,7 @@ module Fedex
     #     @total_net_freight #The freight charge minus dicounts
     #     @total_surcharges #The total amount of all surcharges applied to this shipment
     #     @total_base_charge #The total base charge
-    attr_accessor :service_type, :transit_time, :rate_type, :rate_zone, :total_billing_weight, :total_freight_discounts, :total_net_charge, :total_taxes, :total_net_freight, :total_surcharges, :total_base_charge
+    attr_accessor :service_type, :transit_time, :rate_type, :rate_zone, :total_billing_weight, :total_freight_discounts, :total_net_charge, :total_taxes, :total_net_freight, :total_surcharges, :total_base_charge, :surcharges
     def initialize(options = {})
       @service_type = options[:service_type]
       @transit_time = options[:transit_time]
@@ -32,6 +32,7 @@ module Fedex
       @total_base_charge = options[:total_base_charge][:amount]
       @total_net_fedex_charge = (options[:total_net_fe_dex_charge]||{})[:amount]
       @total_rebates = (options[:total_rebates]||{})[:amount]
+      @surcharges = [options[:surcharges]].flatten
 
       if options[:special_rating_applied] == "FEDEX_ONE_RATE"
         @total_billing_weight = "FEDEX ONE RATE"
